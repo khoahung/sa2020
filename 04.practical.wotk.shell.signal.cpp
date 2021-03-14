25e4ae02a3d628c4e0fcc7efab6c1639878717c7
Nguyễn Tất Hùng <khoahungdev@gmail.com>
2021-03-14 11:33:22 +0700

    update fix bug

--- 04.practical.wotk.shell.signal.cpp
+++ 04.practical.wotk.shell.signal.cpp
@@ -4,34 +4,43 @@
 #include <csignal>
 #include <unistd.h>
 using namespace std;
+int signalPid;
 void signalHandler( int signum ) {
    cout << "Interrupt signal (" << signum << ") received.\n";
-
-   // cleanup and close up stuff here
-   // terminate program
-
-   exit(signum);
+    switch (signum) {
+        case SIGTSTP:
+            cout<<"Pause\n";
+            kill(signalPid,SIGTSTP);
+            break;
+        case SIGINT:
+        case SIGTERM:
+            cout<<"Terminate\n";
+            exit(0);
+            break;
+        default:
+            break;
+    }
 }
 
 
 int main() {
-    int pid = fork();
     signal(SIGINT, signalHandler);
-    if (pid == 0) {
-        printf("enter the command:");
-        char cmd[1000];
-            fgets(cmd,sizeof cmd,stdin);
-
-        char *args[]= {"/bin/bash",cmd,NULL};
-    
-            if(strcmp(cmd,"quit")){
-                    return 0;
+    signal(SIGTERM, signalHandler);
+    signal(SIGTSTP, signalHandler);
+    char cmd[1000];
+    while (1) {
+        cout<<"USShell> ";
+        fgets(cmd,sizeof(cmd),stdin);
+        if(strcmp(cmd,"/q\n")==0) break;
+        int pid = fork();
+        if(pid){
+            signalPid = pid;
+            waitpid(pid, NULL,0);
+        }else{
+            char *args[]= {"/bin/bash","-c",cmd,NULL};
+            execvp("/bin/bash", args);
         }
-        
-        execvp("/bin/bash", args);
-    }else{
-        raise( SIGINT);
-        printf("I am parent after fork(), child is %d\n", pid);
     }
+    cout<<"end";
     return 0;
 }
